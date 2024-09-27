6ccb8088055d1d46dc4472374987759aed81522a
BrentDeClercq <brent.declercq2@student.hogent.be>
2024-09-27 17:20:54 +0200

    more cleanup, can possible go wrong

--- sample-app.sh
+++ sample-app.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+# Cleanup from previous builds
 rm -r tempdir/
+docker stop samplerunning 
+docker rm samplerunning
+
+# Build steps
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
